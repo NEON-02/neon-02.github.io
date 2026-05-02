16f8e9a95525b8f95455dea03a0628f1033b07ce
NEON-02 <bhargavjyoti.1729@gmail.com>
2026-05-03 03:56:02 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+let chats = [
+  { text: "hey mama bear...", side: "left" },
+  { text: "okay this is gonna be a little random 😭", side: "left" },
+  { text: "but i wanted to do something for you", side: "left" },
+  { text: "not like those boring wishes", side: "right" },
+  { text: "so yeah... i made this", side: "right" },
+  { text: "it’s a bit messy", side: "right" },
+  { text: "but it’s very you-coded", side: "right" },
+  { text: "soft… slightly chaotic…", side: "left" },
+  { text: "like you, mommy 😭", side: "right" },
+  { text: "okay continue before i get more cringe", side: "left" }
+];
 
+
+let text = "i don’t say this enough but… you genuinely mean a lot to me 💙 you’re soft in a way that’s comforting, like something safe… like home honestly\n\nHappyyy birthdayyy says snow bear 🐻‍❄️";
+
+
+
+{ text: "okay last thing…", side: "left" }
