c330f5a10bf1daca6b2858a4f3123a145e06f3f7
NEON-02 <bhargavjyoti.1729@gmail.com>
2026-05-14 01:29:43 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+startBtn.addEventListener("click", () => {
 
+    firstPage.style.opacity = "0";
+
+    setTimeout(() => {
+        firstPage.classList.add("hidden");
+
+        secondPage.classList.remove("hidden");
+        secondPage.classList.add("fade");
+
+    }, 500);
+});
+
+const noBtn = document.getElementById("noBtn");
+
+noBtn.addEventListener("mouseover", () => {
+
+    const maxX = 500;
+    const maxY = 120;
+
+    const x = Math.random() * maxX - 200;
+    const y = Math.random() * maxY;
+
+    noBtn.style.left = `${x}px`;
+    noBtn.style.top = `${y}px`;
+});
